2c84d199229fa41a207c3a270312dcbfd42e2414
yanick-vieira <https://github.com/yanick-vieira/coursera-test.git>
2022-12-07 16:49:42 +0000

    js class

---
+++ js/script.js
@@ -0,0 +1,45 @@
+// introdução a javascript
+/*
+console.log("script on head");
+console.log("must be all lowercap");
+
+let a = 1;
+console.log(a);
+*/
+
+//variaveis
+/*
+var test = 3;
+test="yok";
+test="pok"
+var nome ="edukam"
+ 
+console.log(test);
+console.log(nome);
+
+var $cifra ="dollars";
+console.log($cifra);
+
+let testing = 100;
+console.log(testing);
+*/
+ 
+//tipos de dados
+/*
+var num = 5;
+console.log(typeof num);
+
+var float = 4.90;
+console.log(typeof float);
+
+var text = '7.88';
+console.log(typeof text);
+console.log(float);
+console.log(text);
+
+var num = 7;
+console.log(num);
+console.log(typeof num);
+
+console.log(typeof NaN);
+*/
