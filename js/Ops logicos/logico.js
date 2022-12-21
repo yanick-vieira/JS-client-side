1eb9be21305b2ae662f3627802a3df3e56921960
yanick-vieira <yanickvieira7@gmail.com>
2022-12-21 22:27:00 +0000

    operadores logicos

--- js/Ops logicos/logico.js
+++ js/Ops logicos/logico.js
@@ -1,2 +1,13 @@
 //Operadores lógicos
 
+var n = "Yok";
+var i = 25;
+
+if(n == "Yok" && i >= 21){
+    console.log(n + " concedidio a entra na Loja!");
+    console.log("A idade " + i + " é maior que 21!");
+}else{
+    console.log("Deve ter pelo menos 21 anos acima!");
+    console.log(n + " a idade " + i + " é menor que 21!");
+} 
+
