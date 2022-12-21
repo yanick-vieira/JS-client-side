0d442cd186236fb3769b5e7743676a0e92e09db5
yanick-vieira <yanickvieira7@gmail.com>
2022-12-21 23:13:06 +0000

    while by yok

--- js/Repetição/arrays.js
+++ js/Repetição/arrays.js
@@ -8,10 +8,16 @@ while(r <= 3){
 }
 */
 
-ar = ['yok','pok',25,25,'jun',1997];
+ar = ['yok','pok',25,'jun',1997];  console.log(ar)
 var init = 0;
+var next = 1;
+ar[5] = 'keiks';
 
-while(init <= 5){
-    console.log("Start in " + ar[init])
-    init++;
+console.log("Start with " + ar[init])
+
+while(next <= 5){
+    
+    console.log("Next is " + ar[next])
+    next++;
 }
+
