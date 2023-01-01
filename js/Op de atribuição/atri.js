4b742d1ae051a99e1b4e874f22fad9b1ebe79aaa
yanick-vieira <yanickvieira7@gmail.com>
2023-01-01 20:52:40 +0000

    01jan23

---
+++ js/Op de atribuição/atri.js
@@ -0,0 +1,26 @@
+/*
+var x = 1;
+var y = 2;
+
+//soma
+console.log(x = x + y);
+console.log(x += y);
+
+//sub
+console.log(x -= y);
+
+//multi
+console.log(x *= y);
+
+//div
+console.log(x /= y);
+
+//loop
+console.log(x++);
+console.log(x--);
+
+while(x <= 70){
+    console.log(x);
+    x /= 2;
+}
+*/
--- js/Repetição/arrays.js
+++ js/Repetição/arrays.js
@@ -8,6 +8,7 @@ while(r <= 3){
 }
 */
 
+/*
 ar = ['yok','pok',25,'jun',1997];  console.log(ar)
 var init = 0;
 var next = 1;
@@ -20,4 +21,19 @@ while(next <= 5){
     console.log("Next is " + ar[next])
     next++;
 }
+*/
+
+/*
+arrays = ["repeat","again","over again","over and over again"]
+
+var repeat = 0;
+
+while(repeat <= 3){
+    console.log(arrays[0]);
+    arrays[0]++
+}
+*/ 
+
+
+
 
