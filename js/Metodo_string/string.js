75c97177dbdf8e55f0e1a397422d412b140f5ecc
yanick-vieira <yanickvieira7@gmail.com>
2023-01-20 20:40:47 +0000

    arrays

---
+++ js/Metodo_string/string.js
@@ -0,0 +1,23 @@
+/*
+var nome = "Yanick is me myself";
+//length
+console.log(nome.length);
+
+//indexOf
+console.log(nome[7]);
+*/
+
+//Manipular phrases 
+var manipular = "Vou manipular este testo agora!"
+
+var manipulei = manipular.toUpperCase();
+console.log(manipulei.toLowerCase());
+
+var nome = "    Yok   ";
+var nomeTrim = nome.trim();
+//trim
+console.log(nomeTrim);
+
+//lastIndefOf
+console.log(manipular.lastIndexOf("agora"));
+
---
+++ js/Metods_array/mtd_arrays.js
@@ -0,0 +1,14 @@
+var fila = ["eu","ele","aqueles dois","e mais dez", 14];
+
+
+
+//push
+fila.push("chegou mais 3");
+fila.push(17);
+
+//length
+console.log(fila.length);
+
+
+
+
