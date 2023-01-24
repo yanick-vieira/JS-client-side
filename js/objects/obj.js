75964cba757fea57eb58e25db608d4e47a61967b
yanick-vieira <yanickvieira7@gmail.com>
2023-01-24 17:23:08 +0000

    objct

---
+++ js/objects/obj.js
@@ -0,0 +1,14 @@
+//object
+/*
+let service = {
+    service1 : "Social Media",
+    service2 : "Landing Page",
+    service3 : function(){
+        console.log("Traffic Managing");
+    }
+};
+console.log(service.service1);
+*/
+
+
+//object.this
