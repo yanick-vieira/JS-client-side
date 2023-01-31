a3f304945a41b5ea0cffa5acb02ff55390018731
yanick-vieira <yanickvieira7@gmail.com>
2023-01-31 16:50:18 +0000

    dom

---
+++ js/DOM/Interação.js
@@ -0,0 +1,17 @@
+//Alteração diretamente no HTML;
+
+//tag
+var haga_um = document.getElementsByTagName('h1')[0];
+//id
+var paragrafo = document.getElementById('frase');
+//class
+var lista = document.getElementsByClassName('lista')[0];
+//tag
+var lista1 = document.getElementsByTagName('li')[2];
+
+console.log(haga_um);
+console.log(paragrafo);
+console.log(lista);
+console.log(lista1);
+
+
