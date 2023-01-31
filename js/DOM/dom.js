08c15e211aabc320c1fa9362af797e6663520e7f
yanick-vieira <yanickvieira7@gmail.com>
2023-01-31 17:09:40 +0000

    dom

---
+++ js/DOM/dom.js
@@ -0,0 +1,12 @@
+let titulo = document.getElementsByTagName('h1')[0];
+let img = document.getElementsByTagName('img')[0];
+let frase = document.getElementsByTagName('p')[0].style.color = "#fe0000";
+let add = document.getElementById('add');
+
+
+
+
+console.log(frase);
+console.log(titulo);
+console.log(img);
+console.log(add);
